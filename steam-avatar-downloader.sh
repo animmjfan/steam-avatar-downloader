e0b1569a8b7e5152c05b3add3354d2e0c1e9e743
animmjfan <animmjfan@gmail.com>
2026-02-09 08:37:53 +1100

    Instead of outputting to whatever the filename is make it always image.jpg

--- steam-avatar-downloader.sh
+++ steam-avatar-downloader.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 steamCommunityUrl=https://steamcommunity.com
-profileId=YOURUSERRNAMEHERE
+profileId=YOURPROFILEIDHERE
 profile="$steamCommunityUrl/id/$profileId"
 
 imageLocationInSourceHtml=$(curl -s $profile | grep image_src) # example output: <link rel="image_src" href="https://avatars.fastly.steamstatic.com/3ea3ce27118f0eb45f50bfd7a229b17f54ee0a6d_full.jpg">
 setIncludingHref=($imageLocationInSourceHtml) # split into a set of 3 strings including <link, rel="image_src" and href="IMAGEURLHERE">
 link=$( echo ${setIncludingHref[2]%>} | sed 's/href=//; s/"//g' )
-curl -LO $link
+curl $link -O image.jpg
