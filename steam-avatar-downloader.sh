238caa030b8e00752d2e35a6386f6077462bd056
animmjfan <animmjfan@gmail.com>
2026-06-15 15:39:28 +1000

    Fix

--- steam-avatar-downloader.sh
+++ steam-avatar-downloader.sh
@@ -7,4 +7,4 @@ profile="$steamCommunityUrl/id/$profileId"
 imageLocationInSourceHtml=$(curl -s $profile | grep image_src) # example output: <link rel="image_src" href="https://avatars.fastly.steamstatic.com/3ea3ce27118f0eb45f50bfd7a229b17f54ee0a6d_full.jpg">
 setIncludingHref=($imageLocationInSourceHtml) # split into a set of 3 strings including <link, rel="image_src" and href="IMAGEURLHERE">
 link=$( echo ${setIncludingHref[2]%>} | sed 's/href=//; s/"//g' )
-curl $link -O image.jpg
+curl $link -o image.jpg
